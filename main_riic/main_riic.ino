aa61feda7b406b47ab3fc12f71f59c19fe39fc20
andy051200 <bon19451@uvg.edu.gt>
2021-12-09 23:31:23 -0600

    YA JALAAAAAAAA

--- main_riic/main_riic.ino
+++ main_riic/main_riic.ino
@@ -19,11 +19,13 @@ Descripcion: lectura del ADC para sensor de fuerza y se despliegara en un 7seg
 #include <Wire.h>                 //libreria de comunicacion i2c
 #include <Adafruit_SHT31.h>       //libreria de sensor de temperatura
 #include <Adafruit_NeoPixel.h>    //libreria de rueda rgb
+#include <SPIFFS.h>
 /*-----------------------------------------------------------------------------
  ------------ P R O T O T I P O S   D E   F U N C I O N E S -------------------
  -----------------------------------------------------------------------------*/
 void antirrebotes_niveles(void);
 void estado_sensores(void);
+bool HandleFileRead(String);
  /*-----------------------------------------------------------------------------
  -----------------V A R I A B L E S   A   I M P L E M T E N T A R--------------
  -----------------------------------------------------------------------------*/
@@ -35,15 +37,18 @@ void estado_sensores(void);
 #define NUMPIXELS 16                            //numero de leds en aro de luz
 #define DELAYVAL 500 //
 //-------VARIABLES GLOBALES DEL PROGRAMA
-const char* ssid = "Casa Bonilla";              //Enter your SSID here
-const char* password = "losbonilla2021";        //Enter your Password here
+const char* ssid = "Ejemplo RIIC4";              //Enter your SSID here
+const char* password = "123456789";        //Enter your Password here
+IPAddress local_ip(192, 168, 1, 1);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
 WebServer server(80);                           //HTTP port, 80 is defult)
 
-unsigned char nivel1, nivel2, nivel3, niveles;   //variables para sensor Prox
-unsigned char antirrebote1, antirrebote2, antirrebote3; //antirrebotes
-unsigned char aire;                              //variables para aire
-int wenas;
-float temp;                                      //variable de temperatura
+byte nivel1, nivel2, nivel3, niveles;   //variables para sensor Prox
+byte antirrebote1, antirrebote2, antirrebote3; //antirrebotes
+byte aire;                              //variables para aire
+byte wenas;
+float temp,humedad;                                      //variable de temperatura
 bool enableHeater = false;                       //inicializador de temp
 uint8_t loopCnt = 0;
 
@@ -86,21 +91,28 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(5), ISR_n3, FALLING);      //nivel alto
   Serial.begin(9600);
   //-------INICIALIZACION DE SERVIDOR WIFI
-  WiFi.begin(ssid, password);   //se inicia la conexion wifi
-  // Check wi-fi is connected to wi-fi network
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
+  Serial.begin(115200);
+
+  if (!SPIFFS.begin()) {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
   }
-  Serial.println("");
-  Serial.println("WiFi connected successfully");
-  Serial.print("Got IP: ");
-  Serial.println(WiFi.localIP());  //Show ESP32 IP on serial
+  WiFi.softAP(ssid, password);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  delay(100);
+  Serial.print("SSID: ");
+  Serial.println(ssid);
+  Serial.print("password: ");
+  Serial.println(password);
 
-  server.on("/", handle_OnConnect); // Directamente desde e.g. 192.168.0.8
-  server.onNotFound(handle_NotFound);
+  server.on("/", handle_OnConnect); // página de inicio
+  server.onNotFound([]() {                  // si el cliente solicita una uri desconocida
+    if (!HandleFileRead(server.uri()))      // enviar archivo desde SPIFF, si existe
+      handleNotFound();             // sino responder con un error 404 (no existe)
+  });
+  //server.onNotFound(handle_NotFound); //si no se encuentra el uri, responder con error 404 (no existe)
 
-  server.begin();
+  server.begin(); // iniciar servidor
   Serial.println("HTTP server started");
   delay(100);
   //-------INICIALIZACION DE SENSOR 12C
@@ -130,6 +142,7 @@ void loop()
   //-------inicializacion de lecturas
   aire = analogRead(36);              //sensor de co2
   temp = sht31.readTemperature();     //sensor de temperatura
+  humedad = sht31.readHumidity();     //sensor de humedad
   niveles=nivel1+nivel2+nivel3;       
   //-------se inicia el servidor web
   server.handleClient();
@@ -138,7 +151,8 @@ void loop()
   //-------actualizacion de sensores
   estado_sensores();
   //-------ejecucion de pequeño servidor
-  server.send(200, "text/html", SendHTML(aire,temp,niveles));
+  //server.send(200, "text/html", SendHTML(aire,temp,niveles));
+  server.send(200, "text/html", SendHTML(aire,temp,niveles,humedad)); //responde con un OK (200) y envía HTML
   wenas++;
   if (wenas>15){
     wenas=0;
@@ -207,94 +221,189 @@ void estado_sensores(void){
     pixels.show();
     //digitalWrite(12,HIGH);
   }
-  else{
-    pixels.clear();
+  /*else{
+    Serial.print(aire);
+    Serial.print(" | ");
+    Serial.print("no hay basura");
+    Serial.print(" | ");
+    Serial.println(temp);
+    pixels.setPixelColor(wenas,pixels.Color(0 , 0, 0));
     pixels.show();
-  }
+  }*/
  }
 
 //-------HANDLER DE INICIO DE PAGINA
 void handle_OnConnect() {
-  server.send(200, "text/html", SendHTML(aire,temp, niveles));
+  server.send(200, "text/html", SendHTML(aire,temp, niveles,humedad));
 }
 
 //-------funcion para ejecucion de pequeño servidor web
-String SendHTML(unsigned char air, float tempt, unsigned char level) {
+String SendHTML(byte aire, float tempt, byte level, float humedad) {
   String ptr = "<!DOCTYPE html> <html>\n";
+  ptr += " <html>\n";
+  ptr += " <head>\n";
+  ptr += " <meta charset=\"utf-8\">\n";
+  ptr += " <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n";
+  ptr += "<meta http-equiv=\"refresh\" content=\"3\" />";
+  ptr += "<title> YAIMA, RIIC 4.0 val </title>\n";
+  ptr += "<link rel=\"stylesheet\" href=\"index.css\">\n";
+  ptr += " </head>\n";
 
-  ptr += "  <head>\n";
-  ptr += "    <meta chatset=\"utf_8\">\n";
-  ptr += "    <title> YAIMA, RIIC 4.0 </title>\n";
-  ptr += "    <script>\n";
-  ptr += "  <!--\n";
-  ptr += "  function timedRefresh(timeoutPeriod) {\n";
-  ptr += "  \tsetTimeout(\"location.reload(true);\",timeoutPeriod);\n";
-  ptr += "  }\n";
-  ptr += "\n";
-  ptr += "  window.onload = timedRefresh(15000);\n";
-  ptr += "\n";
-  ptr += "  //   -->\n";
-  ptr += "  </script>\n";
-  ptr += "      <h1 align='center'>Proyecto Yaima</h1>\n";
-  ptr += "      <h2 align='center'> C2A2E </h2></th>\n";
-  ptr += "      <h2 align='center'> Cyntia Matus, Crithofer Patzán, Alvaro Sosa, Andy Bonilla, Emmanuel Sandoval</h2></th>\n";
-  //-------prueba de aire
-  if (aire>0){
-    ptr += "<h2 align='center'>hay aire xd</h2>";
-  }
-  else{
-    ptr += "<h2 align='center'>no hay aire xd</h2>";
-  }
-  //-------prueba de temperatura
-  if (temp>0){
-    ptr += "<h2 align='center'>hay temperatura :o </h2>";
-  }
-  else {
-    ptr += "<h2 align='center'>no hay temperatura :o </h2>";
-  }
-  //-------prueba de niveles
-  switch(niveles){
-    case 1: ptr += "<h2 align='center'> nivel 1 </h2>"; break;
-    case 2: ptr += "<h2 align='center'> nivel 2 </h2>"; break;
-    case 3: ptr += "<h2 align='center'> nivel 3 </h2>"; break;
-    default: ptr += "<h2 align='center'> no ta conectado :( </h2>"; break;
-  }
-  /*switch(wenas){
-    case 0:
-      ptr += "<h2 align='center'>Cantidad de Parqueos disponibles: 0</h2>";
-      break;
-    case 1:
-      ptr += "<h2 align='center'>Cantidad de Parqueos disponibles: 1</h2>";
-      break;
-    case 2:
-      ptr += "<h2 align='center'>Cantidad de Parqueos disponibles: 2</h2>";
-      break;
-    case 3:
-      ptr += "<h2 align='center'>Cantidad de Parqueos disponibles: 3</h2>";
-      break;
-    case 4:
-      ptr += "<h2 align='center'>Cantidad de Parqueos disponibles: 4</h2>";
-      break;
-    case 5:
-      ptr += "<h2 align='center'>Cantidad de Parqueos disponibles: 5</h2>";
-      break;
-    case 6:
-      ptr += "<h2 align='center'>Cantidad de Parqueos disponibles: 6</h2>";
-      break;
-    case 7:
-      ptr += "<h2 align='center'>Cantidad de Parqueos disponibles: 7</h2>";
-      break;
-    case 8:
-      ptr += "<h2 align='center'>Cantidad de Parqueos disponibles: 8</h2>";
-      break;
-  }*/
-  ptr += "<img src='https://cdn.pixabay.com/photo/2016/12/31/01/43/auto-1941988_960_720.png'>";
-  ptr += "  </table>\n";
-  ptr += "  </html>\n";
-  ptr += "";
+  ptr += " <body onload=\"gettime()\"><center>";
+  ptr += "     <h1>YAIMA PROJECT</h1>";
+  ptr += " <table style=\"margin-top: 5%;\"><tr>";
+  ptr += "     <td>";
+  ptr += "         <div class=\"content-all\">";
+  ptr += " <div class=\"content-img\">";
+
+  ptr += "             <div class=\"content-txt\">";
+  ptr += "                 <h2> Humedad <br> ";
+  ptr += String(humedad);
+  ptr += "% </h2>";
+  ptr += "            </div>";
+  ptr += "             <div class=\"content-1\"></div>";
+  ptr += "             <div class=\"content-2\"></div>";
+  ptr += "             <div class=\"content-3\"></div>";
+  ptr += "             <div class=\"content-4\"></div>";
+  ptr += "             <img src=\"humedad.jpg \">";
+  ptr += "         </div>";
+  ptr += " </div";
+  ptr += "     </td>";
+  ptr += " <td> <div class=\"content-all\">";
+  ptr += " <div class=\"content-img\">";
+
+  ptr += "             <div class=\"content-txt\">";
+  ptr += "                 <h2> Temperatura <br>";
+  ptr += String(tempt);
+  ptr += "°</h2>";
+  ptr += "             </div>";
+  ptr += "             <div class=\"content-1\"></div>";
+  ptr += "             <div class=\"content-2\"></div>";
+  ptr += "             <div class=\"content-3\"></div>";
+  ptr += "             <div class=\"content-4\"></div>";
+  ptr += "             <img src=\"temperatura.jpg \">";
+  ptr += "         </div> ";
+  ptr += "        </td>";
+  ptr += "        <td> <div class=\"content-all\">";
+  ptr += " <div class=\"content-img\">";
+
+  ptr += "             <div class=\"content-txt\">";
+  ptr += "                 <h2> Nivel de CO<sub>2</sub> <br> ";
+  ptr += String(aire);
+  ptr += "</h2>";
+
+  ptr += "             </div>";
+
+  ptr += "             <div class=\"content-1\"></div>";
+  ptr += "             <div class=\"content-2\"></div>";
+  ptr += "             <div class=\"content-3\"></div>";
+  ptr += "             <div class=\"content-4\"></div>";
+  ptr += "             <img src=\"co4.jpg \">";
+
+  ptr += "         </div> </td> ";
+  ptr += " </tr></table>";
+  ptr += " <table>";
+  ptr += " <tr>";
+  ptr += " <td>";
+  ptr += " <div class=\"content-all\">";
+  ptr += " <div class=\"content-img\">";
+  ptr += "             <div class=\"content-txt\">";
+  ptr += "                 <h2> Mantenimiento<br> </h2>";
+  ptr += "             </div>";
+  ptr += "             <div class=\"content-1\"></div>";
+  ptr += "             <div class=\"content-2\"></div>";
+  ptr += "             <div class=\"content-3\"></div>";
+  ptr += "             <div class=\"content-4\"></div>";
+  ptr += "             <img src=\"settings.png \">";
+  ptr += "         </div></td>";
+  ptr += "         <td><div class=\"content-all\">";
+  ptr += " <div class=\"content-img\">";
+  ptr += "             <div class=\"content-txt\">";
+  ptr += "                 <h2> Capacidad<br>";
+  ptr += String(level);
+  ptr += "</h2>";
+  ptr += "             </div>";
+  ptr += "             <div class=\"content-1\"></div>";
+  ptr += "             <div class=\"content-2\"></div>";
+  ptr += "             <div class=\"content-3\"></div>";
+  ptr += "             <div class=\"content-4\"></div>";
+  ptr += "             <img src=\"bat.jpg \">";
+  ptr += " </div></td>";
+  ptr += " </table>";
+  ptr += " </center>";
+  ptr += " <script language=\"JavaScript\">";
+  ptr += " function gettime() {";
+  ptr += "     var date= new Date();";
+  ptr += "      var s = date.getSeconds();";
+  ptr += "         if(s < 10){";
+  ptr += "         s = \"0\" + s}";
+  ptr += "     document.clockform.clock.value =  s;";
+  ptr += "     setTimeout(\"gettime()\",100)}";
+  ptr += " </script>";
+  ptr += " <form name=\"clockform\">";
+  ptr += " <input type=\"text\" name=\"clock\">";
+  ptr += " </form>";
+  ptr += " </body>";
+  ptr += " </html>";
+  
   return ptr;
 }
-//-------HANDLER DE NOT FOUND
-void handle_NotFound() {
+//************************************************************************************************
+// Handler de not found
+//************************************************************************************************
+void handleNotFound() {
   server.send(404, "text/plain", "Not found");
+}
+//************************************************************************************************
+// Obtener el tipo de contenido del archivo
+//************************************************************************************************
+String GetContentType(String filename)
+{
+  if (filename.endsWith(".htm")) return "text/html";
+  else if (filename.endsWith(".html")) return "text/html";
+  else if (filename.endsWith(".css")) return "text/css";
+  else if (filename.endsWith(".js")) return "application/javascript";
+  else if (filename.endsWith(".png")) return "image/png";
+  else if (filename.endsWith(".gif")) return "image/gif";
+  else if (filename.endsWith(".jpg")) return "image/jpeg";
+  else if (filename.endsWith(".ico")) return "image/x-icon";
+  else if (filename.endsWith(".xml")) return "text/xml";
+  else if (filename.endsWith(".pdf")) return "application/x-pdf";
+  else if (filename.endsWith(".zip")) return "application/x-zip";
+  else if (filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
+//************************************************************************************************
+// Enviar al servidor el archivo desde SPIFFS
+//************************************************************************************************
+void ServeFile(String path)
+{
+  File file = SPIFFS.open(path, "r");
+  size_t sent = server.streamFile(file, GetContentType(path));
+  file.close();
+}
+//************************************************************************************************
+// Enviar al servidor el archivo desde SPIFFS
+//************************************************************************************************
+void ServeFile(String path, String contentType)
+{
+  File file = SPIFFS.open(path, "r");
+  size_t sent = server.streamFile(file, contentType);
+  file.close();
+}
+//************************************************************************************************
+// Handler de not found
+//************************************************************************************************
+bool HandleFileRead(String path)
+{
+  if (path.endsWith("/")) path += "index.html";
+  Serial.println("handleFileRead: " + path);
+
+  if (SPIFFS.exists(path))
+  {
+    ServeFile(path);
+    return true;
+  }
+  Serial.println("\tFile Not Found");
+  return false;
 }
